78ab4ce5d8cc8c9ce45636ae58c39f7f83b98e30
Lianne Maritzer <liannemaritzer@gmail.com>
2017-11-16 16:46:59 -0500

    more animation in unity

--- LEARN_GAME_2/Assets/Scripts/ElementMoveIndividual.cs
+++ LEARN_GAME_2/Assets/Scripts/ElementMoveIndividual.cs
@@ -7,35 +7,45 @@ public class ElementMoveIndividual : MonoBehaviour {
 	public int directNum;
 	public Vector3 direction;
 	public int speed;
+
 	// Use this for initialization
 	void Start () {
-		InvokeRepeating("moveElements", 0.0f, 10.0f);
+        StartCoroutine(moveElements());
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
-		transform.Translate (direction * speed * Time.deltaTime);
+		//transform.Translate (transform.forward * speed * Time.deltaTime);
+
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + transform.forward * 2, speed * Time.deltaTime);
 	}
 
-	void moveElements(){
-		//Debug.Log ("Pick a direction");
-		//for (int j = 0; j < 2; j++) {
-			//for (int i = 0; i < 20; i++) {
-				speed = Random.Range (1, 5);
-				directNum = Random.Range (0, 4);
-				if (directNum == 0) {
-					direction = Vector3.back;
-				} else if (directNum == 1) {
-					direction = Vector3.forward;
-				} else if (directNum == 2) {
-					direction = Vector3.left;
-				} else if (directNum == 3) {
-					direction = Vector3.right;
-				}
-		//Debug.Log (directNum);
-			}
-		//}
-	//}
+	IEnumerator moveElements()
+    {
+        while (true)
+        {
+            speed = Random.Range(1, 5);
+            /*
+            directNum = Random.Range(0, 4);
+            if (directNum == 0) {
+                direction = Vector3.back;
+
+            } else if (directNum == 1) {
+                direction = Vector3.forward;
+
+            } else if (directNum == 2) {
+                direction = Vector3.left;
+
+            } else if (directNum == 3) {
+                direction = Vector3.right;
+            }
+            */
+
+            transform.rotation = Quaternion.Euler(0, Random.Range(0, 360), 0);
+
+            yield return new WaitForSeconds(Random.Range(3.0f, 10.0f));
+        }
+	}
 }
