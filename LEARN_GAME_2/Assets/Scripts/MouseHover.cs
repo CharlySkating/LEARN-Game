6d3a07d02948c43d9886fcfe90751019b7df7673
CharlySkating <charlotte.skating@bell.net>
2017-11-23 12:28:31 -0500

    Electron colors and main menu

---
+++ LEARN_GAME_2/Assets/Scripts/MouseHover.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseHover : MonoBehaviour {
+
+	public bool isStart;
+	public bool isQuit;
+	// Use this for initialization
+	void Start () {
+		GetComponent<Renderer>().material.color = Color.black;
+	}
+
+	void OnMouseEnter() {
+		GetComponent<Renderer>().material.color = Color.red;
+	}
+
+	void OnMouseExit() {
+		GetComponent<Renderer>().material.color = Color.black;
+	}
+
+	void OnMouseUp() {
+		if (isStart) {
+			Debug.Log ("We can start game");
+			Application.LoadLevel ("OpeningEmpty");
+			GetComponent<Renderer>().material.color = Color.cyan;
+		}
+		if (isQuit) {
+			Debug.Log ("We can quit game");
+			Application.Quit ();
+		}
+	}
+}
